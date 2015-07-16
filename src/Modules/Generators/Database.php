b5353be76fbbaafdb5a1debdc1e773b5c6e17115
Mo <Radiergummi@users.noreply.github.com>
2015-07-16 21:57:08 +0200

    Create Database.php

---
+++ src/Modules/Generators/Database.php
@@ -0,0 +1,50 @@
+<?php
+namespace Radiergummi\Cronos\Modules\Generators;
+
+/**
+ * Database class.
+ * Base class for database generators
+ */
+abstract class Database
+{
+	/**
+	 * instance
+	 * 
+	 * (default value: null)
+	 * 
+	 * @var mixed
+	 * @access private
+	 * @static
+	 */
+	private static $instance = null;
+	
+	
+	/**
+	 * instance function.
+	 * 
+	 * @access public
+	 * @static
+	 * @return object  the current database instance
+	 */
+	public static function instance()
+	{
+		// if there is no active connection, start one
+		if (is_null(static::$instance)) static::$instance = static::connect();
+		
+		// return the database instance
+		return static::$instance;
+	}
+
+
+	/**
+	 * connect function.
+	 * 
+	 * @access public
+	 * @static
+	 * @return object  the PDO
+	 */
+	public static function connect()
+	{
+		return new PDO();
+	}
+}
