e92d2ab6ca12fc2f942baa281a2f9d6d29df8de4
Mo <Radiergummi@users.noreply.github.com>
2015-07-16 21:56:33 +0200

    Create Folder.php

---
+++ src/Modules/Generators/Folder.php
@@ -0,0 +1,53 @@
+<?php
+namespace Radiergummi\Cronos\Modules\Generators;
+
+/**
+ * Folder class.
+ * 
+ * @implements Module
+ * @implements Generator
+ */
+class Folder implements Generator
+{
+	/**
+	 * excludes
+	 * 
+	 * (default value: array())
+	 * 
+	 * @var array
+	 * @access public
+	 * @static
+	 */
+	public static $excludes = array();
+
+
+	public function __construct()
+	{
+		
+	}
+
+
+	/**
+	 * getDirectory function.
+	 * recursively iterates over a directory
+	 * 
+	 * @access private
+	 * @param mixed $path
+	 * @return void
+	 */
+	private function getDirectory($path)
+	{
+		$files = array();
+		
+		$di = new RecursiveDirectoryIterator($path, RecursiveDirectoryIterator::SKIP_DOTS);
+		foreach (new RecursiveIteratorIterator($di) as $name => $file) {
+
+			// skip file if its a folder, they are being included anyway
+			if ($excludeEmptyFolders && $file->isDir()) continue;
+
+			
+		}
+		
+		return $files;
+	}
+}
