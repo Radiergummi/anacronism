c8bfc137fdccd0be1ce8f061c2f7ce6268b23a11
Mo <Radiergummi@users.noreply.github.com>
2015-07-27 22:39:11 +0200

    Create Dropbox.php

---
+++ src/Modules/Writers/Dropbox.php
@@ -0,0 +1,15 @@
+<?php
+namespace Radiergummi\Anacronism\Modules\Writers;
+
+/**
+ * Dropbox class.
+ * 
+ * @implements Radiergummi\Anacronism\Modules\Writer
+ */
+class Dropbox implements Radiergummi\Anacronism\Modules\Writer
+{
+  public function write($files)
+  {
+    # TODO
+  }
+}
