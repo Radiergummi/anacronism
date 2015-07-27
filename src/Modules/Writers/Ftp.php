9207d2f9b0faec008c1ebc70cc7755953ce9d755
Mo <Radiergummi@users.noreply.github.com>
2015-07-27 22:38:16 +0200

    Create Ftp.php

---
+++ src/Modules/Writers/Ftp.php
@@ -0,0 +1,15 @@
+<?php
+namespace Radiergummi\Anacronism\Modules\Writers;
+
+/**
+ * Ftp class.
+ * 
+ * @implements Radiergummi\Anacronism\Modules\Writer
+ */
+class Ftp implements Radiergummi\Anacronism\Modules\Writer
+{
+  public function write($files)
+  {
+    # TODO
+  }
+}
