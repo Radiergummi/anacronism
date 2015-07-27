63d158de77b97b44a933d81ea18c30072e7df871
Mo <Radiergummi@users.noreply.github.com>
2015-07-27 22:38:33 +0200

    Create Scp.php

---
+++ src/Modules/Writers/Scp.php
@@ -0,0 +1,15 @@
+<?php
+namespace Radiergummi\Anacronism\Modules\Writers;
+
+/**
+ * Scp class.
+ * 
+ * @implements Radiergummi\Anacronism\Modules\Writer
+ */
+class Scp implements Radiergummi\Anacronism\Modules\Writer
+{
+  public function write($files)
+  {
+    # TODO
+  }
+}
