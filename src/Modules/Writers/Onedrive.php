889fd44930dda0c1d9b8521a27caf373ff6e8c4b
Mo <Radiergummi@users.noreply.github.com>
2015-07-27 22:38:52 +0200

    Create Onedrive.php

---
+++ src/Modules/Writers/Onedrive.php
@@ -0,0 +1,15 @@
+<?php
+namespace Radiergummi\Anacronism\Modules\Writers;
+
+/**
+ * Onedrive class.
+ * 
+ * @implements Radiergummi\Anacronism\Modules\Writer
+ */
+class Onedrive implements Radiergummi\Anacronism\Modules\Writer
+{
+  public function write($files)
+  {
+    # TODO
+  }
+}
